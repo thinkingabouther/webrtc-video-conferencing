e8f531aa96b528e0a2319f52a280ff3aad13ad8a
Arseny Neustroev <neustroev.arseny@gmail.com>
2022-06-17 21:17:27 +0700

    Update dbConnection.js

--- server/dbal/dbConnection.js
+++ server/dbal/dbConnection.js
@@ -2,7 +2,7 @@ const arangojs = require("arangojs");
 const dbConfig = require("../config.js").db;
 const dbConnection = arangojs({
   url: dbConfig.url,
-  databaseName: "webrtc",
+  databaseName: "p2p-videoconferencing",
 });
 
 module.exports = dbConnection.useBasicAuth(dbConfig.user, dbConfig.password);
